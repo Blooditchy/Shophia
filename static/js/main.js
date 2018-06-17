1f40b093d6d954cb46bb36bcc323ee130118ddc7
Evgeny Chistyakov <just15@yandex.ru>
2018-06-17 23:15:08 +0300

    Первый коммит

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slider__list').bxSlider({
+        controls: false,
+        prevText: '',
+        nextText: '',
+        pagerCustom: '#bx-pager'
+    });
+});
